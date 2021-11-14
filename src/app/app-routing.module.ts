d8028199157a6704a8c80389dc0ebd58d4840119
badcodeenjoyer <somanyskillsinme@gmail.com>
2021-11-15 01:34:22 +0200

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+//import {AppComponent} from "./app.component";
+import {CarFormComponent} from "./car-form/car-form.component";
+import {ErrorComponent} from "./error/error.component";
+import {HomeComponent} from "./home/home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',component:HomeComponent},
+  {path:'carForm',component:CarFormComponent},
+  {path:'carForm/:id',component:CarFormComponent},
+  {path:'carForm/:id/:status',component:CarFormComponent},
+  {path: 'error', component: ErrorComponent},
+  {path: '**', redirectTo: '/error'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {CommonModule} from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import {MainServiceService} from "./services/main-service.service";
+import { CarFormComponent } from './car-form/car-form.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ErrorComponent } from './error/error.component';
+import {FormsModule} from "@angular/forms";
+import { HomeComponent } from './home/home.component';
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarFormComponent,
+    ErrorComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule,
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(MainServiceService),
+    NgbModule,
+    ReactiveFormsModule,
+    FormsModule,
+    CommonModule,
+    ReactiveFormsModule.withConfig({warnOnNgModelWithFormControl: 'never'}),
   ],
   providers: [],
   bootstrap: [AppComponent]
